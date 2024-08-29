a1ef3bc68ce772a5f12dc8cbd0c87f21e9a3acb8
reeceomahoney <reece.omahoney3@gmail.com>
2024-08-29 19:42:52 +0100

    Add latex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://reeceomahoney.github.io',
+  site: "https://reeceomahoney.github.io",
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
